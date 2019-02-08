8e0b0cb38890e61db0913a8cb6bf0be6369fa7b3
Mai Thanh Minh <thanhminh.mr@gmail.com>
2019-02-08 14:57:51 +0700

    Update not-paid.js

--- not-paid.js
+++ not-paid.js
@@ -1,23 +1,14 @@
-(function(){
-	/* change these variables as you wish */
-	var due_date = new Date('2017-02-27');
-	var days_deadline = 60;
-	/* stop changing here */
-	
-	var current_date = new Date();
-	var utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
-	var utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
-	var days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
-	
-	if(days > 0) {
-		var days_late = days_deadline-days;
-		var opacity = (days_late*100/days_deadline)/100;
-			opacity = (opacity < 0) ? 0 : opacity;
-			opacity = (opacity > 1) ? 1 : opacity;
-		if(opacity >= 0 && opacity <= 1) {
-			document.getElementsByTagName("BODY")[0].style.opacity = opacity;
-		}
-		
-	}
-	
-})()
+// MIT
+(function(due_date_string, days_deadline = 60) {
+  var due_date = new Date(due_date_string).valueOf();
+  var current_date = new Date().valueOf();
+  var days = Math.floor((current_date - due_date) / (1000 * 60 * 60 * 24));
+  if (days > 0) {
+    var opacity = 1 - days / days_deadline;
+    opacity = (opacity < 0) ? 0 : opacity;
+    opacity = (opacity > 1) ? 1 : opacity;
+    if (opacity >= 0 && opacity <= 1) {
+      document.getElementsByTagName('body')[0].style.setProperty('opacity', opacity, 'important');
+    }
+  }
+})('2019-01-01')
